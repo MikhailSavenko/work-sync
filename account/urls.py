9d0c837ca13b16af9fdd664450ae53256e85c346
Mike <misha47savenko@yandex.ru>
2025-05-15 14:14:20 +0300

    urls logout account refresh to blacklis

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-
+from rest_framework_simplejwt.views import TokenBlacklistView
 app_name = "account"
 
 urlpatterns = [
+    path("auth/jwt/logout/", TokenBlacklistView.as_view(), name="logout"),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
 ]
