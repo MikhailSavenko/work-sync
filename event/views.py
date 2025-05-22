3c1991dd112c6f283cba2d011b991918889c4f88
Mike <misha47savenko@yandex.ru>
2025-05-22 17:02:44 +0300

    meentings/me

--- event/views.py
+++ event/views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.utils import timezone
 
 from event.models import Meeting
 from event.serializers import MeetingSerializer
@@ -13,4 +16,14 @@ class MeetingViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         current_user = self.request.user.worker
         serializer.save(creator=current_user)
+    
+    @action(detail=False, methods=["get"], url_path="me")
+    def me(self, request):
+        user_worker = request.user.worker
+        now = timezone.now()
 
+        meetings = Meeting.objects.filter(workers=user_worker, datetime__gt=now)
+        serializer = self.serializer_class(meetings, many=True)
+
+        return Response(serializer.data)
+        
