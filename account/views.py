b983f9a0120cca0bd6e24aa0463eddf3fc92dacb
Mike <misha47savenko@yandex.ru>
2025-05-26 12:14:58 +0300

    calendar_day in  WorkerViewSet

--- account/views.py
+++ account/views.py
@@ -1,9 +1,20 @@
 from rest_framework import viewsets
 from rest_framework import mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+
+from datetime import datetime, time
+
 
 from account.serializers import TeamCreateUpdateSerializer, WorkerGetSerializer, TeamGetSerializer
 from account.models import Team, Worker
 
+from event.models import Meeting
+from task.models import Task
+from event.serializers import MeetingSerializer
+from task.serializers import GetTaskSerializer
 
 class TeamViewSet(viewsets.ModelViewSet):
     """Представление для Team"""
@@ -24,7 +35,32 @@ class TeamViewSet(viewsets.ModelViewSet):
 class WorkerViewSet(viewsets.GenericViewSet,
                     mixins.RetrieveModelMixin,
                     mixins.ListModelMixin):
+    permission_classes = (IsAuthenticated,)
     serializer_class = WorkerGetSerializer
     queryset = Worker.objects.all()
+
+    @action(detail=False, methods=["get"], url_path="calendar/day/(?P<date>\\d{4}-\\d{2}-\\d{2})")
+    def calendar_day(self, request, date):
+        """
+        Эндпоиинт просмотра событий сотрудника за день 
+        date - обязательный параметр пути YYYY-MM-DD
+        """
+        worker = request.user.worker
+
+        parse_date = datetime.strptime(date, "%Y-%m-%d").date()
+        start = datetime.combine(parse_date, time.min)
+        end = datetime.combine(parse_date, time.max)
+
+        meetings = Meeting.objects.filter(workers=worker, datetime__range=(start, end))
+        tasks = Task.objects.filter(executor=worker, deadline__range=(start, end))
+
+        meeting_serializer_data = MeetingSerializer(meetings, many=True)
+        tasks_serializer_data = GetTaskSerializer(tasks, many=True, context={"request": request})
+
+        return Response(data={
+            "date": parse_date,
+            "meetings": meeting_serializer_data.data,
+            "tasks": tasks_serializer_data.data
+            })
 
     
